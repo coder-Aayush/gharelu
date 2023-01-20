de67f072248bc50a5fbc7de61d2f2980497a535b
coder-Aayush <bhattaraiaayush08@gmail.com>
2023-01-21 03:19:08 +0545

    Fix

--- lib/src/core/widgets/cache_image_viewer.dart
+++ lib/src/core/widgets/cache_image_viewer.dart
@@ -5,9 +5,9 @@ import 'package:gharelu/src/core/extensions/context_extension.dart';
 
 class CacheImageViewer extends StatelessWidget ***REMOVED***
   const CacheImageViewer(
-      ***REMOVED***Key? key, required this.imageUrl, this.height, this.width, this.fit***REMOVED***)
+      ***REMOVED***Key? key, this.imageUrl, this.height, this.width, this.fit***REMOVED***)
       : super(key: key);
-  final String imageUrl;
+  final String? imageUrl;
   final double? height;
   final double? width;
   final BoxFit? fit;
@@ -15,12 +15,13 @@ class CacheImageViewer extends StatelessWidget ***REMOVED***
 ***REMOVED***
   Widget build(BuildContext context) ***REMOVED***
     return CachedNetworkImage(
-      imageUrl: imageUrl,
+      imageUrl: imageUrl ?? '',
       progressIndicatorBuilder: (_, url, downloadProgress) ***REMOVED***
         return url.isEmpty
             ? Image.asset(Assets.images.logo.path, fit: BoxFit.cover)
             : context.loader;
       ***REMOVED***,
+      errorWidget: (context, url, error) => const Icon(Icons.error_outline),
       fit: fit ?? BoxFit.cover,
       height: height,
       width: width,
--- lib/src/home/widgets/search_services.dart
+++ lib/src/home/widgets/search_services.dart
@@ -1,5 +1,10 @@
 import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gharelu/src/core/extensions/extensions.dart';
+import 'package:gharelu/src/core/routes/app_router.dart';
+import 'package:gharelu/src/core/widgets/widgets.dart';
+import 'package:gharelu/src/home/models/category_model.dart';
 
 class CustomSearchServices extends SearchDelegate ***REMOVED***
   CustomSearchServices(this.searcher);
@@ -19,9 +24,17 @@ class CustomSearchServices extends SearchDelegate ***REMOVED***
 ***REMOVED***
   Widget buildResults(BuildContext context) ***REMOVED***
     searcher.query(query);
+    return Container();
+  ***REMOVED***
+
+***REMOVED***
+  Widget buildSuggestions(BuildContext context) ***REMOVED***
+    searcher.query(query);
+
     return StreamBuilder(
       stream: searcher.responses,
       builder: (context, AsyncSnapshot<SearchResponse> snapshot) ***REMOVED***
+        print(snapshot);
         final response = snapshot.data;
         final hits = response?.hits.toList() ?? [];
         return ListView.builder(
@@ -30,20 +43,31 @@ class CustomSearchServices extends SearchDelegate ***REMOVED***
             final hit = hits[index];
 
             return ListTile(
+              leading: GradientCircle(
+                radius: 40,
+                child: CacheImageViewer(imageUrl: (hit['image'] as String?)),
+              ),
+              onTap: () => context.router.push(
+                CategoryRoute(
+                  category: CategoryModel(
+                    id: hit['category_id'].toString(),
+                    name: hit['name'].toString(),
+                    image: hit['image'].toString(),
+                    createdAt: hit['created_at'] as int,
+                    updatedAt: hit['1672337769716'] as int,
+                    enable: true,
+                  ),
+                ),
+              ),
               title: RichText(
                   text: TextSpan(
                 style: Theme.of(context).textTheme.subtitle2,
-                children: hit.getHighlightedString('title').toInlineSpans(),
+                children: hit.getHighlightedString('name').toInlineSpans(),
               )),
-            );
+            ).py(12.h);
           ***REMOVED***,
         );
       ***REMOVED***,
     );
-  ***REMOVED***
-
-***REMOVED***
-  Widget buildSuggestions(BuildContext context) ***REMOVED***
-    return Container();
   ***REMOVED***
 ***REMOVED***
